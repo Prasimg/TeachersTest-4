0c61a021e071dd86daad5bbf26bb955c935994ce
Pratham <prasimg@gmail.com>
2022-10-16 01:01:52 +0530

    Set up route for canvas component

--- src/App.js
+++ src/App.js
@@ -7,16 +7,34 @@ import Hero from "./components/Hero";
 import Canvas from "./components/Canvas";
 import About from "./components/About";
 import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-            <Navbar />
-            <Hero />
-            <Form />
-            <Canvas width={700} height={500} />
-            <About />
-            <Footer />
+           <BrowserRouter>
+           <Routes>
+                <Route path="/" element={
+                    <>
+                     <Navbar />
+                     <Hero />
+                      <Form />
+                    
+                      <About />
+                      <Footer />
+                    </>
+                }></Route>
+
+                <Route path="/canvas" element={
+                    <>
+                    <Navbar />
+                    <Canvas width={700} height={500} />
+                    <Footer />
+                    </>
+
+                }></Route>
+           </Routes>
+           </BrowserRouter>
         </div>
     );
 }
